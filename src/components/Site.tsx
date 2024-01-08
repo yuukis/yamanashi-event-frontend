59f61bf8d2a4ca4e8e140ebeffb5fda38ebb68ab
Yuuki Shimizu <yuuki@maxio.jp>
2024-01-08 11:33:13 +0900

    add calendar icon

--- src/components/Site.tsx
+++ src/components/Site.tsx
@@ -1,5 +1,5 @@
-import { Heading, Stack, Spacer, Link } from '@chakra-ui/react';
-import { Github } from '@chakra-icons/bootstrap';
+import { Flex, Heading, Stack, Spacer, Link, Text } from '@chakra-ui/react';
+import { Github, Calendar2Plus } from '@chakra-icons/bootstrap';
   
 export function SiteHeader(prop: any) {
 
@@ -11,6 +11,8 @@ export function SiteHeader(prop: any) {
         <Heading size={{base: 'sm', md: 'md'}} fontWeight={'normal'}>{ title }</Heading>
       </Link>
       <Spacer />
+      <Calendar2Plus boxSize={{base: '5', md: '6'}} />
+      <Text fontSize={{base: 'xs', md: 'sm'}} mr={'2'}>カレンダーに追加</Text>
       <Link href={'https://github.com/yuukis/yamanashi-event-frontend'} target={'_blank'}>
         <Github boxSize={{base: '5', md: '6'}} />
       </Link>
