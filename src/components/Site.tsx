7aa53f384ab09dbab579f8bdfb9f23bd528de7cd
Yuuki Shimizu <yuuki@maxio.jp>
2024-01-08 19:47:12 +0900

    add popover content

--- src/components/Site.tsx
+++ src/components/Site.tsx
@@ -3,8 +3,12 @@ import {
   Stack,
   Spacer,
   Link,
+  List,
+  ListItem,
+  ListIcon,
   Text,
-  Button, 
+  Button,
+  Input,
   Popover,
   PopoverTrigger,
   PopoverContent,
@@ -13,7 +17,7 @@ import {
   PopoverCloseButton,
   PopoverBody
 } from '@chakra-ui/react';
-import { Github, Calendar3 } from '@chakra-icons/bootstrap';
+import { Github, Calendar3, CaretRightFill } from '@chakra-icons/bootstrap';
   
 export function SiteHeader(prop: any) {
 
@@ -45,10 +49,35 @@ export function ICalendarButton() {
       </PopoverTrigger>
       <PopoverContent>
         <PopoverArrow />
-        <PopoverHeader>iCalendar で外部のカレンダーと連携</PopoverHeader>
+        <PopoverHeader>
+          <Text fontSize={'sm'}>
+            iCalendar で外部のカレンダーに表示する
+          </Text>
+        </PopoverHeader>
         <PopoverCloseButton />
         <PopoverBody>
-          <Text fontSize={'sm'}>https://calendar.yamanashi.dev/event.ics</Text>
+          <Stack>
+            <Text fontSize={'xs'}>
+              以下の URL をコピーして、Google カレンダーなどのカレンダーに登録すると、
+              カレンダー上でイベントが表示されるようになります
+            </Text>
+            <Input value={'https://event.yamanashi.dev/calendar.ics'}
+                  size={'sm'}
+                  contentEditable
+                  onSelect={(e) => {
+                    const inputElement = e.target as HTMLInputElement;
+                    inputElement.select();
+                  }}
+                  />
+            <List fontSize={'sm'} p={'2'}>
+              <ListItem>
+                <ListIcon as={CaretRightFill} />
+                <Link href={'https://calendar.google.com/calendar/r/settings/addbyurl'} target={'_blank'}>
+                  Google カレンダーに登録する
+                </Link>
+              </ListItem>
+            </List>
+          </Stack>
         </PopoverBody>
       </PopoverContent>
     </Popover>
