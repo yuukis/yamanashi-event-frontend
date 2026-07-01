1ba6f7ad641436080c28dc1d80db7c23c52a9642
Yuuki Shimizu <yuuki@maxio.jp>
2026-07-01 20:51:39 +0900

    イベントメニューによる横スクロールを抑止

--- src/components/EventBody.tsx
+++ src/components/EventBody.tsx
@@ -380,30 +380,34 @@ export function EventBody(data: EventBodyProps) {
                   </HStack>
                 </Button>
                 <Box h="full" w="1px" />
-                <Menu placement="bottom-end">
-                  <MenuButton as={IconButton}
-                              aria-label='Options'
-                              icon={<ChevronDownIcon />}
-                              />
-                  <MenuList fontSize={'sm'}>
-                    <MenuItem icon={<FiExternalLink />}
-                              onClick={() => window.open(event_url)}
-                              >
-                      情報提供元のページを開く
-                    </MenuItem>
-                    <MenuItem icon={<FaXTwitter />}
-                              onClick={() => window.open(event_x_search_url)}
-                              >
-                      イベント当日の X(Twitter) 投稿を検索
-                    </MenuItem>
-                    {archive_url && (
-                      <MenuItem icon={<FiArchive />}
-                                onClick={() => window.open(archive_url)}
-                                >
-                        アーカイブ元を開く
-                      </MenuItem>
-                    )}
-                  </MenuList>
+                <Menu placement="bottom-end" isLazy>
+                  {({ isOpen: isMenuOpen }) => (
+                    <>
+                      <MenuButton as={IconButton}
+                                  aria-label='Options'
+                                  icon={<ChevronDownIcon />}
+                                  />
+                      <MenuList fontSize={'sm'} display={isMenuOpen ? 'block' : 'none'}>
+                        <MenuItem icon={<FiExternalLink />}
+                                  onClick={() => window.open(event_url)}
+                                  >
+                          情報提供元のページを開く
+                        </MenuItem>
+                        <MenuItem icon={<FaXTwitter />}
+                                  onClick={() => window.open(event_x_search_url)}
+                                  >
+                          イベント当日の X(Twitter) 投稿を検索
+                        </MenuItem>
+                        {archive_url && (
+                          <MenuItem icon={<FiArchive />}
+                                    onClick={() => window.open(archive_url)}
+                                    >
+                            アーカイブ元を開く
+                          </MenuItem>
+                        )}
+                      </MenuList>
+                    </>
+                  )}
                 </Menu>
               </ButtonGroup>
             </Show>
