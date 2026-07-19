c6e141b10180e6bacdb5615fd6ad6b482132df51
Yuuki Shimizu <yuuki@maxio.jp>
2026-07-19 09:06:25 +0900

    git repo に yamanashi-event-sync-backend を追加

--- src/components/Site.tsx
+++ src/components/Site.tsx
@@ -554,6 +554,12 @@ export function GithubButton() {
             <Spacer />
           </Button>
           <Button w={'100%'} variant={'ghost'} size={'sm'}
+                  onClick={() => { window.open('https://github.com/yuukis/yamanashi-event-sync-backend', '_blank') }}>
+            <Github mr={'2'} />
+            <Text fontWeight={'normal'}>yuukis/yamanashi-event-sync-backend</Text>
+            <Spacer />
+          </Button>
+          <Button w={'100%'} variant={'ghost'} size={'sm'}
                   onClick={() => { window.open('https://github.com/yuukis/yamanashi-event-feed', '_blank') }}>
             <Github mr={'2'} />
             <Text fontWeight={'normal'}>yuukis/yamanashi-event-feed</Text>
