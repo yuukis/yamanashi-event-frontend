513906e6b7c85f062ea3f1f9623e2faee188bff8
Yuuki Shimizu <yuuki@maxio.jp>
2026-07-30 01:14:04 +0900

    feat: 次回イベント予定バナー用のフィールド定義とパスビルダーを追加

--- src/utils/widgetFields.ts
+++ src/utils/widgetFields.ts
@@ -11,3 +11,18 @@ export const WIDGET_EVENT_ITEM_FIELDS = [
   'address',
   'group_name',
 ].join(',');
+
+export const WIDGET_NEXT_EVENT_FIELDS = [
+  'uid',
+  'title',
+  'event_url',
+  'started_at',
+  'ended_at',
+  'open_status',
+].join(',');
+
+export const WIDGET_GROUP_FIELDS = [
+  'key',
+  'title',
+  'image_url',
+].join(',');
